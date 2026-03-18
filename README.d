e6e35c57c10e9cbbb761c3c0c6adf98b302b2443
Benrich-Martin-Barry <benrichmartin@gmail.com>
2026-03-18 13:24:33 +0530

    Create README.d

---
+++ README.d
@@ -0,0 +1,31 @@
+# Linux Log Analyzer
+
+## Overview
+
+This project analyzes Linux log files using Python to detect error messages and generate reports.
+
+## Features
+
+* Reads server log files
+* Detects error messages
+* Counts total errors
+* Generates automated report
+
+## Technologies Used
+
+* Python
+* Linux
+* File Handling
+* Git & GitHub
+
+## How to Run
+
+1. Navigate to project folder
+2. Run:
+   python3 log_analyzer.py
+
+## Output
+
+* Displays error messages
+* Shows total error count
+* Saves result in log_report.txt
